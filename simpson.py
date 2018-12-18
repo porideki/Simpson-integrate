9fc85245e283935ce19035051ce13baf6cabe99a
porideki <porideki@gmail.com>
2018-12-18 15:39:06 +0900

    完成
    完成した

--- simpson.py
+++ simpson.py
@@ -1,13 +1,15 @@
 def simpson(bottom, top, span):
 
-    n = (top - bottom) // span
-    sections = []
-
-    for i in range((n / 2) - 1):
-        sections = (f(bottom + span * 2 * i) + 4 * f(bottom + span * 2 * (i + 1) + f(bottom + span * 2 * (i + 2))))
+        area = 0
         
-    
+        for i in range(int((top - bottom) / (span * 2))):
+                area += (span / 3) * (f(bottom + span * 2 * i) + 4 * f(bottom + span * (2 * i + 1)) + f(bottom + span * (2 * i + 2)))
+
+        return area
 
 def f(x):
-    import math
-    return math.sin((math.pi * x)/3)
+        import math
+        return math.sin((math.pi * x) / 3)
+    
+if __name__ == "__main__":
+        print(simpson(0, 3, 0.5))
